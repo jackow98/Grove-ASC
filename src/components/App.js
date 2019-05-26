0bec18c96d06c11b152abb5730b2580cc8e8a092
jackow98 <jw50@hw.ac.uk>
2019-05-26 14:45:20 +0100

    Added lambda function to communicate with dynamoDB for storing members

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ import Achievements from "./memberPages/Achievements";
 import News from "./memberPages/News";
 import Calendar from "./memberPages/Calendar";
 
-import Amplify from 'aws-amplify';
+import Amplify, {Analytics, API} from 'aws-amplify';
 import awsmobile from '../aws-exports';
 import { withAuthenticator } from 'aws-amplify-react';
 
@@ -20,10 +20,33 @@ Amplify.configure(awsmobile);
 //The root of the app detailing navigation to each page
 class App extends React.Component {
 
+    post = async () => {
+        console.log('calling api');
+        const response = await API.post('groveRestapi', '/items', {
+            body: {
+                SASANumber: '1'
+            }
+        });
+        alert(JSON.stringify(response, null, 2));
+    };
+    get = async () => {
+        console.log('calling api');
+        const response = await API.get('groveRestapi', '/items/object/1');
+        alert(JSON.stringify(response, null, 2));
+    };
+    list = async () => {
+        console.log('calling api');
+        const response = await API.get('groveRestapi', '/items/1');
+        alert(JSON.stringify(response, null, 2));
+    };
 
     render() {
         return (
             <BrowserRouter>
+
+                <button onClick={this.post}>POST</button>
+                <button onClick={this.get}>GET</button>
+                <button onClick={this.list}>LIST</button>
 
                 {/*Public Pages*/}
                 <Route path="/" exact component={PublicHome}/>
